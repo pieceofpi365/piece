b88c0b925dfdd0510a3f4b64d0b45878f6880b36
Piece Collector <80719398+pieceofpi365@users.noreply.github.com>
2021-08-01 09:54:18 -0700

    Update main.h

--- src/main.h
+++ src/main.h
@@ -54,7 +54,7 @@ static const int64 DUST_SOFT_LIMIT = 10000; // 0.0001 PIPI
 /** Dust Hard Limit, ignored as wallet inputs (mininput default) */
 static const int64 DUST_HARD_LIMIT = 1;   // 0.00000001 PIPI mininput
 /** No amount larger than this (in satoshi) is valid */
-static const int64 MAX_MONEY = 4357784 * COIN;
+static const int64 MAX_MONEY = 4357780 * COIN;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 1;
